e86b6c2871d4fe4260531d57e2c449569e4dd0ee
abhishek <aabhiojha08@Outlook.com>
2026-03-20 10:34:31 +0545

    Add RoomMemberMapper and RoomMemberDto

--- src/main/java/com/abhishekojha/kurakanimonolith/modules/room_member/dto/RoomMemberDto.java
+++ src/main/java/com/abhishekojha/kurakanimonolith/modules/room_member/dto/RoomMemberDto.java
@@ -3,12 +3,23 @@ package com.abhishekojha.kurakanimonolith.modules.room_member.dto;
 import com.abhishekojha.kurakanimonolith.modules.room.model.Room;
 import com.abhishekojha.kurakanimonolith.modules.room_member.model.RoomRole;
 import com.abhishekojha.kurakanimonolith.modules.user.AppUser;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class RoomMemberDto {
     private Long roomMemberId;
+    @NotNull
     private Long roomId;
+    @NotNull
     private Long userId;
     private RoomRole roomRole;
     private LocalDateTime joinedAt;
--- src/main/java/com/abhishekojha/kurakanimonolith/modules/room_member/mapper/RoomMemberMapper.java
+++ src/main/java/com/abhishekojha/kurakanimonolith/modules/room_member/mapper/RoomMemberMapper.java
@@ -1,11 +1,31 @@
 package com.abhishekojha.kurakanimonolith.modules.room_member.mapper;
 
+import com.abhishekojha.kurakanimonolith.modules.room.dto.RoomDto;
+import com.abhishekojha.kurakanimonolith.modules.room.model.Room;
+import com.abhishekojha.kurakanimonolith.modules.room_member.dto.RoomMemberDto;
+import com.abhishekojha.kurakanimonolith.modules.room_member.model.RoomMember;
+import com.abhishekojha.kurakanimonolith.modules.user.AppUser;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RoomMemberMapper {
 
+    @Mapping(source = "room", target="roomId")
+    @Mapping(source = "user", target="userId")
+    RoomMemberDto toDto(RoomMember room);
 
+    List<RoomMemberDto> toDto(List<RoomMember> rooms);
+
+    default Long mapRoomToRoomId(Room room){
+        return room.getId();
+    }
+
+    default Long mapUserToUserId(AppUser user){
+        return user.getId();
+    }
 
 }
