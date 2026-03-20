524f9f168c2b97c54e4131210c3dbfd245fbb400
abhishek <aabhiojha08@Outlook.com>
2026-03-20 10:35:19 +0545

    Add MessageDto, MessageMapper and MessageController

---
+++ src/main/java/com/abhishekojha/kurakanimonolith/modules/room/dto/CreateRoomRequestDto.java
@@ -0,0 +1,22 @@
+package com.abhishekojha.kurakanimonolith.modules.room.dto;
+
+import com.abhishekojha.kurakanimonolith.modules.room.model.RoomType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateRoomRequestDto {
+    @NotNull
+    @NotBlank
+    @Size(max = 100)
+    private String name;
+    @NotNull
+    @Size(max = 255)
+    private String description;
+    @NotNull
+    private RoomType type;
+}
