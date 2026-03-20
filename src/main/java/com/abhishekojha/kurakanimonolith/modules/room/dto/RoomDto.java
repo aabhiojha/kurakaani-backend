8dae94386a4bef2ca0a8c3a4f777a2dafeded894
abhishek <aabhiojha08@Outlook.com>
2026-03-20 10:34:43 +0545

    Update RoomDto, RoomMapper and add MessageDto

--- src/main/java/com/abhishekojha/kurakanimonolith/modules/room/dto/RoomDto.java
+++ src/main/java/com/abhishekojha/kurakanimonolith/modules/room/dto/RoomDto.java
@@ -1,23 +1,35 @@
 package com.abhishekojha.kurakanimonolith.modules.room.dto;
 
+import com.abhishekojha.kurakanimonolith.modules.message.dto.MessageDto;
 import com.abhishekojha.kurakanimonolith.modules.message.model.Message;
 import com.abhishekojha.kurakanimonolith.modules.room.model.RoomType;
 import com.abhishekojha.kurakanimonolith.modules.room_member.model.RoomMember;
 import com.abhishekojha.kurakanimonolith.modules.user.AppUser;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class RoomDto {
 
     private Long id;
     private String name;
     private String description;
     private List<RoomMember> members;
-    private List<Message> messages;
+    private List<MessageDto> messageDtos;
     private RoomType type;
-    private AppUser created_by;
-    private LocalDateTime created_at;
-    private LocalDateTime updated_at;
+    @NotNull
+    private Long createdById;
+    @NotNull
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
 }
--- src/main/java/com/abhishekojha/kurakanimonolith/modules/room/mapper/RoomMapper.java
+++ src/main/java/com/abhishekojha/kurakanimonolith/modules/room/mapper/RoomMapper.java
@@ -1,11 +1,32 @@
 package com.abhishekojha.kurakanimonolith.modules.room.mapper;
 
+import com.abhishekojha.kurakanimonolith.modules.message.dto.MessageDto;
+import com.abhishekojha.kurakanimonolith.modules.message.mapper.MessageMapper;
+import com.abhishekojha.kurakanimonolith.modules.message.model.Message;
+import com.abhishekojha.kurakanimonolith.modules.room.dto.RoomDto;
+import com.abhishekojha.kurakanimonolith.modules.room.model.Room;
+import com.abhishekojha.kurakanimonolith.modules.user.AppUser;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+import java.util.List;
+
+@Mapper(componentModel = "spring",
+        uses = MessageMapper.class,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RoomMapper {
 
+    @Mapping(source = "createdBy", target = "createdById", qualifiedByName = "createdByToCreatedById")
+    @Mapping(source = "messages", target = "messageDtos")
+    RoomDto toDto(Room room);
 
+    List<RoomDto> toDtoList(List<Room> messages);
+
+    @Named("createdByToCreatedById")
+    default Long mapToCreatedById(AppUser createdBy) {
+        return createdBy.getId();
+    }
 
 }
