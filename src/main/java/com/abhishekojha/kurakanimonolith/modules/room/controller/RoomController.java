7b27beef365434f5a3ac929fd0de8d5dd71aecd6
abhishek <aabhiojha08@Outlook.com>
2026-03-22 14:13:45 +0545

    add member endpoints at room

--- src/main/java/com/abhishekojha/kurakanimonolith/modules/room/controller/RoomController.java
+++ src/main/java/com/abhishekojha/kurakanimonolith/modules/room/controller/RoomController.java
@@ -1,26 +1,36 @@
 package com.abhishekojha.kurakanimonolith.modules.room.controller;
 
+import com.abhishekojha.kurakanimonolith.modules.room.dto.AddUsersToRoomDto;
 import com.abhishekojha.kurakanimonolith.modules.room.dto.CreateRoomRequestDto;
 import com.abhishekojha.kurakanimonolith.modules.room.dto.RoomDto;
 import com.abhishekojha.kurakanimonolith.modules.room.service.RoomService;
 import com.abhishekojha.kurakanimonolith.modules.room.service.RoomServiceImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/rooms")
 @RequiredArgsConstructor
 public class RoomController {
 
-    private final RoomService roomService;
+    private final RoomServiceImpl roomService;
 
     @PreAuthorize("hasRole('USER')")
     @PostMapping
-    public RoomDto createNewRoom(@RequestBody CreateRoomRequestDto createRoomRequestDto){
+    public RoomDto createNewRoom(@Valid @RequestBody CreateRoomRequestDto createRoomRequestDto){
         return roomService.createRoom(createRoomRequestDto);
+    }
+
+    @PostMapping("/room/{room_id}")
+    public ResponseEntity<?> addUsersToRoom(
+            @Valid @RequestBody AddUsersToRoomDto addUsersToRoomDto,
+            @PathVariable Long room_id
+    ){
+         roomService.addUserToRoom(addUsersToRoomDto, room_id);
+         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/abhishekojha/kurakanimonolith/modules/room/service/RoomServiceImpl.java
+++ src/main/java/com/abhishekojha/kurakanimonolith/modules/room/service/RoomServiceImpl.java
@@ -1,19 +1,31 @@
 package com.abhishekojha.kurakanimonolith.modules.room.service;
 
+import com.abhishekojha.kurakanimonolith.common.exception.exceptions.BadRequestException;
+import com.abhishekojha.kurakanimonolith.common.exception.exceptions.DuplicateResourceException;
+import com.abhishekojha.kurakanimonolith.common.exception.exceptions.ResourceNotFoundException;
+import com.abhishekojha.kurakanimonolith.common.exception.exceptions.UnauthorizedException;
 import com.abhishekojha.kurakanimonolith.common.security.SecurityUtils;
+import com.abhishekojha.kurakanimonolith.modules.room.dto.AddUsersToRoomDto;
 import com.abhishekojha.kurakanimonolith.modules.room.dto.CreateRoomRequestDto;
 import com.abhishekojha.kurakanimonolith.modules.room.dto.RoomDto;
 import com.abhishekojha.kurakanimonolith.modules.room.mapper.RoomMapper;
 import com.abhishekojha.kurakanimonolith.modules.room.model.Room;
 import com.abhishekojha.kurakanimonolith.modules.room.repository.RoomRepository;
+import com.abhishekojha.kurakanimonolith.modules.room_member.model.RoomMember;
+import com.abhishekojha.kurakanimonolith.modules.room_member.model.RoomRole;
+import com.abhishekojha.kurakanimonolith.modules.room_member.repository.RoomMemberRepository;
 import com.abhishekojha.kurakanimonolith.modules.user.AppUser;
-import jakarta.validation.Valid;
+import com.abhishekojha.kurakanimonolith.modules.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,15 +35,23 @@ public class RoomServiceImpl implements RoomService {
     private final RoomRepository roomRepository;
     private final RoomMapper roomMapper;
     private final SecurityUtils securityUtils;
+    private final UserRepository userRepository;
+    private final RoomMemberRepository roomMemberRepository;
 
+    @PreAuthorize("hasRole('USER')")
     @Override
     @Transactional
-    public RoomDto createRoom(@Valid CreateRoomRequestDto createRoomRequestDto) {
+    public RoomDto createRoom(CreateRoomRequestDto createRoomRequestDto) {
         // user
         AppUser requestUser = securityUtils.getRequestUser();
-
         if (requestUser == null) {
-            return null;
+            throw new UnauthorizedException("The user not authenticated.");
+        }
+        // check if the room of same name exists already.
+        List<Room> roomList = roomRepository.findByCreatedByAndNameIgnoreCase(requestUser, createRoomRequestDto.getName());
+
+        if (!roomList.isEmpty()) {
+            throw new DuplicateResourceException("The room with name '%s' already exists".formatted(createRoomRequestDto.getName()));
         }
 
         log.info("The request user is retrieved: {}", requestUser.getEmail());
@@ -43,13 +63,76 @@ public class RoomServiceImpl implements RoomService {
         room.setType(createRoomRequestDto.getType());
         room.setCreatedBy(requestUser);
 
-        Room save = roomRepository.save(room);
-        log.info("The room is created {}", save);
-        return roomMapper.toDto(save);
+        Room savedRoom = roomRepository.save(room);
+        log.info("Room created. id={}, name={}, createdBy={}",
+                savedRoom.getId(), savedRoom.getName(), requestUser.getEmail());
+        return roomMapper.toDto(savedRoom);
     }
 
     @Override
     public List<RoomDto> getRooms() {
+
         return List.of();
+    }
+
+    @Override
+    @Transactional
+    public void addUserToRoom(AddUsersToRoomDto request, Long room_id) {
+        List<Long> userIds = request.getUserIds();
+
+        // check if the room exists
+        Room room = roomRepository.findById(room_id).orElseThrow(
+                () -> new ResourceNotFoundException("Room not found"));
+
+        if (userIds.isEmpty()) {
+            throw new BadRequestException("The list of users is empty.");
+        }
+
+        // check if the users actually exist
+        List<AppUser> usersList = userRepository.findAllById(userIds);
+
+        // all the user ids from db
+        List<Long> fetchedUserIds = usersList.stream()
+                .map(AppUser::getId)
+                .toList();
+
+        // missing entries in db
+        List<Long> missingUserIds = userIds.stream()
+                .filter(id -> !fetchedUserIds.contains(id))
+                .toList();
+
+        // check if there are any users that do not exist in the db
+        if (!missingUserIds.isEmpty()) {
+            throw new ResourceNotFoundException("User with id '%s' not found".formatted(missingUserIds));
+        }
+
+        // create room member entry
+        for (AppUser user : usersList) {
+            RoomMember member = RoomMember.builder()
+                    .room(room).user(user).roomRole(RoomRole.MEMBER).build();
+
+        }
+
+        List<RoomMember> newMembers = usersList.stream()
+                .map(user -> RoomMember.builder()
+                        .room(room)
+                        .user(user)
+                        .roomRole(RoomRole.MEMBER)
+                        .build())
+                .toList();
+        List<RoomMember> roomMembers = roomMemberRepository.saveAll(newMembers);
+        log.info("The room members {} are added in the room", roomMembers);
+    }
+
+    @Override
+    @Transactional
+    public void removeUserFromRoom() {
+
+    }
+
+    @Override
+    @Transactional
+    public void deleteRoom() {
+
     }
 }
